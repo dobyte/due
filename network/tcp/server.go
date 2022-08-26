056e2ae72da987a02fa170287e38d4454cd60e87
fuxiao <576101059@qq.com>
2022-08-26 14:13:35 +0800

    support tcp network protocol

---
+++ network/tcp/server.go
@@ -0,0 +1,141 @@
+/**
+ * @Author: fuxiao
+ * @Email: 576101059@qq.com
+ * @Date: 2022/5/11 10:02 上午
+ * @Desc: TODO
+ */
+
+package tcp
+
+import (
+	"net"
+	"time"
+
+	"github.com/dobyte/due/network"
+)
+
+type server struct {
+	opts              *serverOptions            // 配置
+	listener          net.Listener              // 监听器
+	connMgr           *serverConnMgr            // 连接管理器
+	startHandler      network.StartHandler      // 服务器启动hook函数
+	stopHandler       network.CloseHandler      // 服务器关闭hook函数
+	connectHandler    network.ConnectHandler    // 连接打开hook函数
+	disconnectHandler network.DisconnectHandler // 连接关闭hook函数
+	receiveHandler    network.ReceiveHandler    // 接收消息hook函数
+}
+
+var _ network.Server = &server{}
+
+func NewServer(opts ...ServerOption) network.Server {
+	o := &serverOptions{
+		addr:         ":3553",
+		maxConnNum:   5000,
+		maxMsgLength: 1024 * 1024,
+	}
+	for _, opt := range opts {
+		opt(o)
+	}
+
+	s := &server{}
+	s.opts = o
+	s.connMgr = newConnMgr(s)
+
+	return s
+}
+
+// Addr 监听地址
+func (s *server) Addr() string {
+	return s.opts.addr
+}
+
+// Start 启动服务器
+func (s *server) Start() error {
+	if err := s.init(); err != nil {
+		return err
+	}
+
+	if s.startHandler != nil {
+		s.startHandler()
+	}
+
+	go s.serve()
+
+	return nil
+}
+
+// Stop 关闭服务器
+func (s *server) Stop() error {
+	if err := s.listener.Close(); err != nil {
+		return err
+	}
+
+	s.connMgr.close()
+
+	return nil
+}
+
+// Protocol 协议
+func (s *server) Protocol() string {
+	return "tcp"
+}
+
+// OnStart 监听服务器启动
+func (s *server) OnStart(handler network.StartHandler) {
+	s.startHandler = handler
+}
+
+// OnStop 监听服务器关闭
+func (s *server) OnStop(handler network.CloseHandler) {
+	s.stopHandler = handler
+}
+
+// OnConnect 监听连接打开
+func (s *server) OnConnect(handler network.ConnectHandler) {
+	s.connectHandler = handler
+}
+
+// OnDisconnect 监听连接关闭
+func (s *server) OnDisconnect(handler network.DisconnectHandler) {
+	s.disconnectHandler = handler
+}
+
+// OnReceive 监听接收到消息
+func (s *server) OnReceive(handler network.ReceiveHandler) {
+	s.receiveHandler = handler
+}
+
+// 初始化TCP服务器
+func (s *server) init() error {
+	addr, err := net.ResolveTCPAddr("tcp", s.opts.addr)
+	if err != nil {
+		return err
+	}
+
+	ln, err := net.ListenTCP(addr.Network(), addr)
+	if err != nil {
+		return err
+	}
+
+	s.listener = ln
+
+	return nil
+}
+
+// 等待连接
+func (s *server) serve() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			if e, ok := err.(net.Error); ok && e.Timeout() {
+				time.Sleep(time.Millisecond)
+				continue
+			}
+			return
+		}
+
+		if err := s.connMgr.allocate(conn); err != nil {
+			_ = conn.Close()
+		}
+	}
+}
