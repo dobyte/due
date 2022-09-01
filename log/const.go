5d27a6a8054fb3c4e86fa5abb74b0afd8b8a9c0f
fuxiao <576101059@qq.com>
2022-09-01 12:58:35 +0800

    modify cut rule name

--- log/const.go
+++ log/const.go
@@ -19,10 +19,10 @@ const (
 type CutRule int
 
 const (
-	YearCutRule   CutRule = iota // 按照年切割
-	MonthCutRule                 // 按照月切割
-	DayCutRule                   // 按照日切割
-	HourCutRule                  // 按照时切割
-	MinuteCutRule                // 按照分切割
-	SecondCutRule                // 按照秒切割
+	CutByYear   CutRule = iota // 按照年切割
+	CutByMonth                 // 按照月切割
+	CutByDay                   // 按照日切割
+	CutByHour                  // 按照时切割
+	CutByMinute                // 按照分切割
+	CutBySecond                // 按照秒切割
 )
