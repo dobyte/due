8cfe429f17a5f93e9d809785260a08f9346e6b5c
fuxiao <576101059@qq.com>
2022-09-03 10:49:09 +0800

    fix log writer bug

--- log/writer.go
+++ log/writer.go
@@ -34,6 +34,8 @@ func NewWriter(opts WriterOptions) (io.Writer, error) {
 		fileExt      string
 		fileName     string
 		rotationTime time.Duration
+		srcFileParts = make([]string, 0, 3)
+		newFileParts = make([]string, 0, 4)
 	)
 
 	path, file := filepath.Split(opts.Path)
@@ -51,37 +53,40 @@ func NewWriter(opts WriterOptions) (io.Writer, error) {
 		fileName, fileExt = strings.Join(list[:c-1], "."), list[c-1]
 	}
 
-	fileParts := make([]string, 0, 4)
-	fileParts = append(fileParts, fileName)
+	srcFileParts = append(srcFileParts, fileName)
+	newFileParts = append(newFileParts, fileName)
+
 	if opts.Level != 0 {
-		fileParts = append(fileParts, strings.ToLower(opts.Level.String()))
+		srcFileParts = append(srcFileParts, strings.ToLower(opts.Level.String()))
+		newFileParts = append(newFileParts, strings.ToLower(opts.Level.String()))
 	}
 
 	switch opts.CutRule {
 	case CutByYear:
-		fileParts = append(fileParts, "%Y")
+		newFileParts = append(newFileParts, "%Y")
 		rotationTime = 365 * 24 * time.Hour
 	case CutByMonth:
-		fileParts = append(fileParts, "%Y%m")
+		newFileParts = append(newFileParts, "%Y%m")
 		rotationTime = 31 * 24 * time.Hour
 	case CutByDay:
-		fileParts = append(fileParts, "%Y%m%d")
+		newFileParts = append(newFileParts, "%Y%m%d")
 		rotationTime = 24 * time.Hour
 	case CutByHour:
-		fileParts = append(fileParts, "%Y%m%d%H")
+		newFileParts = append(newFileParts, "%Y%m%d%H")
 		rotationTime = time.Hour
 	case CutByMinute:
-		fileParts = append(fileParts, "%Y%m%d%H%M")
+		newFileParts = append(newFileParts, "%Y%m%d%H%M")
 		rotationTime = time.Minute
 	case CutBySecond:
-		fileParts = append(fileParts, "%Y%m%d%H%M%S")
+		newFileParts = append(newFileParts, "%Y%m%d%H%M%S")
 		rotationTime = time.Second
 	}
 
-	fileParts = append(fileParts, fileExt)
+	srcFileParts = append(srcFileParts, fileExt)
+	newFileParts = append(newFileParts, fileExt)
 
-	srcFileName := filepath.Join(path, fileName+"."+fileExt)
-	newFileName := filepath.Join(path, strings.Join(fileParts, "."))
+	srcFileName := filepath.Join(path, strings.Join(srcFileParts, "."))
+	newFileName := filepath.Join(path, strings.Join(newFileParts, "."))
 
 	options := make([]rotatelogs.Option, 0, 4)
 	options = append(options, rotatelogs.WithLinkName(srcFileName))
