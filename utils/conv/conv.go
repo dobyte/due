c5161b6c2f2bbfef67a14f6ef3903aa70ee644e7
fuxiao <576101059@qq.com>
2022-10-09 18:29:36 +0800

    cache

--- utils/conv/conv.go
+++ utils/conv/conv.go
@@ -390,6 +390,25 @@ func Byte(any interface{}) byte {
 	return Uint8(any)
 }
 
+func Bytes(any interface{}) []byte {
+	if any == nil {
+		return nil
+	}
+
+	switch v := any.(type) {
+	case string:
+		return []byte(v)
+	case *string:
+		return []byte(*v)
+	case []byte:
+		return v
+	case *[]byte:
+		return *v
+	default:
+		return nil
+	}
+}
+
 func Bool(any interface{}) bool {
 	if any == nil {
 		return false
