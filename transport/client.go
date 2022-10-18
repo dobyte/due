070d556fa51ae16722873c455476fc79de08328e
fuxiao <576101059@qq.com>
2022-10-18 19:14:29 +0800

    extract transporter interface and remove grpc plugin

--- transport/client.go
+++ transport/client.go
@@ -2,73 +2,36 @@ package transport
 
 import (
 	"context"
+	"github.com/dobyte/due/cluster"
+	"github.com/dobyte/due/session"
 )
-
-type Client interface {
-	Call(ctx context.Context)
-}
 
 type NodeClient interface {
 	// Trigger 触发事件
-	Trigger(ctx context.Context, req *TriggerRequest) (*TriggerReply, error)
+	Trigger(ctx context.Context, event cluster.Event, gid string, uid int64) (miss bool, err error)
 	// Deliver 投递消息
-	Deliver(ctx context.Context, req *DeliverRequest) (*DeliverReply, error)
+	Deliver(ctx context.Context, gid, nid string, cid, uid int64, message *Message) (miss bool, err error)
 }
 
 type GateClient interface {
 	// Bind 绑定用户与连接
-	Bind(ctx context.Context, req *BindRequest) (*BindReply, error)
+	Bind(ctx context.Context, cid, uid int64) (miss bool, err error)
 	// Unbind 解绑用户与连接
-	Unbind(ctx context.Context, req *UnbindRequest) (*UnbindReply, error)
-}
-
-type TriggerRequest struct {
-	Event int32  // 事件
-	GID   string // 网关ID
-	UID   int64  // 用户ID
-}
-
-type TriggerReply struct {
-}
-
-type DeliverRequest struct {
-	GID     string   // 网关ID
-	NID     string   // 节点ID
-	CID     int64    // 连接ID
-	UID     int64    // 用户ID
-	Message *Message // 消息
-}
-
-type DeliverReply struct {
+	Unbind(ctx context.Context, uid int64) (miss bool, err error)
+	// GetIP 获取客户端IP
+	GetIP(ctx context.Context, nid string, kind session.Kind, target int64) (ip string, miss bool, err error)
+	// Disconnect 断开连接
+	Disconnect(ctx context.Context, nid string, kind session.Kind, target int64, isForce bool) (miss bool, err error)
+	// Push 推送消息
+	Push(ctx context.Context, nid string, kind session.Kind, target int64, message *Message) (miss bool, err error)
+	// Multicast 推送组播消息
+	Multicast(ctx context.Context, nid string, kind session.Kind, targets []int64, message *Message) (total int64, err error)
+	// Broadcast 推送广播消息
+	Broadcast(ctx context.Context, nid string, kind session.Kind, message *Message) (total int64, err error)
 }
 
 type Message struct {
 	Seq    int32  // 序列号
 	Route  int32  // 路由
 	Buffer []byte // 消息内容
-}
-
-type BindRequest struct {
-	CID int64 // 连接ID
-	UID int64 // 用户ID
-}
-
-type BindReply struct {
-}
-
-type UnbindRequest struct {
-	UID int64 // 用户ID
-}
-
-type UnbindReply struct {
-}
-
-type GetIPRequest struct {
-	NID    string // 节点ID
-	Kind   int32  // 推送类型 1：CID 2：UID
-	Target int64  // 推送目标
-}
-
-type GetIPReply struct {
-	IP string // IP地址
 }
