ae85d611b6fd086b3cf129742045903f9fc4cab1
fuxiao <576101059@qq.com>
2022-10-24 12:58:33 +0800

    Optimize grpc client

--- transport/grpc/internal/client/client.go
+++ transport/grpc/internal/client/client.go
@@ -15,7 +15,7 @@ type Options struct {
 }
 
 func Dial(opts *Options) (*grpc.ClientConn, error) {
-	options := make([]grpc.DialOption, 0)
+	options := make([]grpc.DialOption, 1)
 
 	if opts.IsSecure {
 		if opts.CertFile == "" {
