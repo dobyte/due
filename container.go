514af00776873d153ada740aea3c7e6e248a9689
fuxiao <576101059@qq.com>
2022-11-10 17:36:50 +0800

    adjust container

--- container.go
+++ container.go
@@ -12,13 +12,12 @@ import (
 
 type Container struct {
 	sig        chan os.Signal
-	die        chan struct{}
 	components []component.Component
 }
 
 // NewContainer 创建一个容器
 func NewContainer() *Container {
-	return &Container{sig: make(chan os.Signal), die: make(chan struct{})}
+	return &Container{sig: make(chan os.Signal)}
 }
 
 // Add 添加组件
@@ -28,7 +27,7 @@ func (c *Container) Add(components ...component.Component) {
 
 // Serve 启动容器
 func (c *Container) Serve() {
-	debugPrint()
+	log.Debug("Welcome to the due framework, Learn more at https://github.com/dobyte/due")
 
 	for _, comp := range c.components {
 		comp.Init()
@@ -38,42 +37,16 @@ func (c *Container) Serve() {
 		comp.Start()
 	}
 
-	defer func() {
-		log.Warn("container will close")
-		signal.Stop(c.sig)
-		close(c.die)
-		for _, comp := range c.components {
-			comp.Destroy()
-		}
-	}()
-
 	signal.Notify(c.sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGKILL, syscall.SIGTERM, syscall.SIGUSR1, syscall.SIGUSR2)
 
-RESERVE:
-	select {
-	case sig := <-c.sig:
-		log.Warnf("container got signal %v", sig)
-		switch sig {
-		case syscall.SIGHUP, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGKILL, syscall.SIGTERM: // 监听关闭
-			return
-		case syscall.SIGUSR1, syscall.SIGUSR2: // 监听重启
-			for _, comp := range c.components {
-				comp.Restart()
-			}
+	sig := <-c.sig
 
-			goto RESERVE
-		}
-	case <-c.die:
-		return
-	}
-}
+	log.Warnf("process got signal %v, container will close", sig)
 
-// Close 关闭容器
-func (c *Container) Close() {
-	c.die <- struct{}{}
+	signal.Stop(c.sig)
 	config.Close()
-}
 
-func debugPrint() {
-	log.Debug("Welcome to the due framework, Learn more at https://github.com/dobyte/due")
+	for _, comp := range c.components {
+		comp.Destroy()
+	}
 }
