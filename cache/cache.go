7e8a1fddbbad50bbf35b1b92550b4c64c8df9def
fuxiao <576101059@qq.com>
2023-12-28 12:13:59 +0800

    added cache module

--- cache/cache.go
+++ cache/cache.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+var globalCache Cache
+
 type SetValueFunc func() (interface{}, time.Duration, error)
 
 type Cache interface {
@@ -30,4 +32,69 @@ type Cache interface {
 	AddPrefix(key string) string
 	// Client 获取客户端
 	Client() interface{}
+}
+
+// SetCache 设置缓存
+func SetCache(cache Cache) {
+	globalCache = cache
+}
+
+// GetCache 获取缓存
+func GetCache() Cache {
+	return globalCache
+}
+
+// Has 检测缓存是否存在
+func Has(ctx context.Context, key string) (bool, error) {
+	return globalCache.Has(ctx, key)
+}
+
+// Get 获取缓存值
+func Get(ctx context.Context, key string, def ...interface{}) Result {
+	return globalCache.Get(ctx, key, def)
+}
+
+// Set 设置缓存值
+func Set(ctx context.Context, key string, value interface{}, expiration ...time.Duration) error {
+	return globalCache.Set(ctx, key, value, expiration...)
+}
+
+// GetSet 获取设置缓存值
+func GetSet(ctx context.Context, key string, fn SetValueFunc) Result {
+	return globalCache.GetSet(ctx, key, fn)
+}
+
+// Delete 删除缓存
+func Delete(ctx context.Context, key string) (bool, error) {
+	return globalCache.Delete(ctx, key)
+}
+
+// IncrInt 整数自增
+func IncrInt(ctx context.Context, key string, value int64) (int64, error) {
+	return globalCache.IncrInt(ctx, key, value)
+}
+
+// IncrFloat 浮点数自增
+func IncrFloat(ctx context.Context, key string, value float64) (float64, error) {
+	return globalCache.IncrFloat(ctx, key, value)
+}
+
+// DecrInt 整数自减
+func DecrInt(ctx context.Context, key string, value int64) (int64, error) {
+	return globalCache.DecrInt(ctx, key, value)
+}
+
+// DecrFloat 浮点数自减
+func DecrFloat(ctx context.Context, key string, value float64) (float64, error) {
+	return globalCache.DecrFloat(ctx, key, value)
+}
+
+// AddPrefix 添加Key前缀
+func AddPrefix(key string) string {
+	return globalCache.AddPrefix(key)
+}
+
+// Client 获取客户端
+func Client() interface{} {
+	return globalCache.Client()
 }
