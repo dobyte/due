749db1bef014d09f2576fcbb777aeba2ebbb0d29
fuxiao <576101059@qq.com>
2024-05-01 14:36:24 +0800

    added internal transport protocol

---
+++ internal/stream/internal/protocol/broadcast_test.go
@@ -0,0 +1,45 @@
+package protocol_test
+
+import (
+	"github.com/dobyte/due/v2/internal/stream/internal/codes"
+	"github.com/dobyte/due/v2/internal/stream/internal/protocol"
+	"github.com/dobyte/due/v2/session"
+	"testing"
+)
+
+func TestEncodeBroadcastReq(t *testing.T) {
+	buffer := protocol.EncodeBroadcastReq(1, session.User, []byte("hello world"))
+
+	t.Log(buffer.Bytes())
+}
+
+func TestDecodeBroadcastReq(t *testing.T) {
+	buffer := protocol.EncodeBroadcastReq(1, session.User, []byte("hello world"))
+
+	seq, kind, message, err := protocol.DecodeBroadcastReq(buffer.Bytes())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("seq: %v", seq)
+	t.Logf("kind: %v", kind)
+	t.Logf("message: %v", string(message))
+}
+
+func TestEncodeBroadcastRes(t *testing.T) {
+	buffer := protocol.EncodeBroadcastRes(1, codes.OK, 20)
+
+	t.Log(buffer.Bytes())
+}
+
+func TestDecodeBroadcastRes(t *testing.T) {
+	buffer := protocol.EncodeBroadcastRes(1, codes.OK, 20)
+
+	code, total, err := protocol.DecodeBroadcastRes(buffer.Bytes())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("code: %v", code)
+	t.Logf("total: %v", total)
+}
---
+++ internal/stream/internal/protocol/trigger.go
@@ -0,0 +1,110 @@
+package protocol
+
+import (
+	"encoding/binary"
+	"github.com/dobyte/due/v2/cluster"
+	"github.com/dobyte/due/v2/core/buffer"
+	"github.com/dobyte/due/v2/errors"
+	"github.com/dobyte/due/v2/internal/stream/internal/route"
+	"io"
+)
+
+const (
+	triggerReqBytes = defaultSizeBytes + defaultHeaderBytes + defaultRouteBytes + defaultSeqBytes + b8 + b64 + b64
+	triggerResBytes = defaultSizeBytes + defaultHeaderBytes + defaultRouteBytes + defaultSeqBytes + defaultCodeBytes
+)
+
+// EncodeTriggerReq 编码触发事件请求
+// 协议：size + header + route + seq + event + cid + [uid]
+func EncodeTriggerReq(seq uint64, event cluster.Event, cid int64, uid ...int64) buffer.Buffer {
+	size := triggerReqBytes - defaultSizeBytes
+	if len(uid) == 0 || uid[0] == 0 {
+		size -= b64
+	}
+
+	buf := buffer.NewNocopyBuffer()
+	writer := buf.Malloc(triggerReqBytes)
+	writer.WriteUint32s(binary.BigEndian, uint32(size))
+	writer.WriteUint8s(dataBit)
+	writer.WriteUint8s(route.Trigger)
+	writer.WriteUint64s(binary.BigEndian, seq)
+	writer.WriteUint8s(uint8(event))
+	writer.WriteInt64s(binary.BigEndian, cid)
+
+	if len(uid) > 0 && uid[0] != 0 {
+		writer.WriteInt64s(binary.BigEndian, uid[0])
+	}
+
+	return buf
+}
+
+// DecodeTriggerReq 解码触发事件请求
+// 协议：size + header + route + seq + event + cid + [uid]
+func DecodeTriggerReq(data []byte) (seq uint64, event cluster.Event, cid int64, uid int64, err error) {
+	if len(data) != triggerReqBytes && len(data) != triggerReqBytes-b64 {
+		err = errors.ErrInvalidMessage
+		return
+	}
+
+	reader := buffer.NewReader(data)
+
+	if _, err = reader.Seek(defaultSizeBytes+defaultHeaderBytes+defaultRouteBytes, io.SeekStart); err != nil {
+		return
+	}
+
+	if seq, err = reader.ReadUint64(binary.BigEndian); err != nil {
+		return
+	}
+
+	var evt uint8
+	if evt, err = reader.ReadUint8(); err != nil {
+		return
+	} else {
+		event = cluster.Event(evt)
+	}
+
+	if cid, err = reader.ReadInt64(binary.BigEndian); err != nil {
+		return
+	}
+
+	if len(data) == triggerReqBytes {
+		uid, err = reader.ReadInt64(binary.BigEndian)
+	}
+
+	return
+}
+
+// EncodeTriggerRes 编码触发事件响应
+// 协议：size + header + route + seq + code
+func EncodeTriggerRes(seq uint64, code uint16) buffer.Buffer {
+	buf := buffer.NewNocopyBuffer()
+	writer := buf.Malloc(triggerResBytes)
+	writer.WriteUint32s(binary.BigEndian, uint32(triggerResBytes-defaultSizeBytes))
+	writer.WriteUint8s(dataBit)
+	writer.WriteUint8s(route.Trigger)
+	writer.WriteUint64s(binary.BigEndian, seq)
+	writer.WriteUint16s(binary.BigEndian, code)
+
+	return buf
+}
+
+// DecodeTriggerRes 解码触发事件响应
+// 协议：size + header + route + seq + code
+func DecodeTriggerRes(data []byte) (code uint16, err error) {
+	if len(data) != triggerResBytes {
+		err = errors.ErrInvalidMessage
+		return
+	}
+
+	reader := buffer.NewReader(data)
+
+	if _, err = reader.Seek(-defaultCodeBytes, io.SeekEnd); err != nil {
+		return
+	}
+
+	if code, err = reader.ReadUint16(binary.BigEndian); err != nil {
+		return
+	}
+
+	return
+}
