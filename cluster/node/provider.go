3e737591029851f53810393d07bcc96595e4c6d7
fuxiao <576101059@qq.com>
2025-01-06 20:25:14 +0800

    fixed bug

--- cluster/node/provider.go
+++ cluster/node/provider.go
@@ -13,35 +13,6 @@ type provider struct {
 
 // Trigger 触发事件
 func (p *provider) Trigger(ctx context.Context, gid string, cid, uid int64, event cluster.Event) error {
-	switch event {
-	case cluster.Connect:
-		// ignore
-	case cluster.Reconnect:
-		if uid == 0 {
-			return errors.ErrInvalidArgument
-		}
-
-		_, ok, err := p.node.proxy.AskNode(ctx, uid, p.node.opts.name, p.node.opts.id)
-		if err != nil {
-			return err
-		}
-
-		if !ok {
-			return errors.ErrNotFoundSession
-		}
-	case cluster.Disconnect:
-		if uid != 0 {
-			_, ok, err := p.node.proxy.AskNode(ctx, uid, p.node.opts.name, p.node.opts.id)
-			if err != nil {
-				return err
-			}
-
-			if !ok {
-				return errors.ErrNotFoundSession
-			}
-		}
-	}
-
 	p.node.trigger.trigger(event, gid, cid, uid)
 
 	return nil
