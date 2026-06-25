33efe2d85deb8124955aff105b16e1238025a21e
fuxiao <yuebanfuxiao@gmail.com>
2026-06-25 20:32:45 +0800

    Fixed a bug that prevented Grpc load balancing.

--- transport/grpc/internal/resolver/direct/builder.go
+++ transport/grpc/internal/resolver/direct/builder.go
@@ -14,14 +14,14 @@ const scheme = "direct"
 
 type Builder struct {
 	rw        sync.RWMutex
-	states    map[string]resolver.State
+	states    map[string]*resolver.State
 	resolvers sync.Map
 }
 
 var _ resolver.Builder = &Builder{}
 
 func NewBuilder() *Builder {
-	return &Builder{states: make(map[string]resolver.State)}
+	return &Builder{states: make(map[string]*resolver.State)}
 }
 
 func (b *Builder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
@@ -34,7 +34,7 @@ func (b *Builder) Build(target resolver.Target, cc resolver.ClientConn, opts res
 		state := b.states[target.URL.Host]
 		b.rw.RUnlock()
 
-		r.updateState(state)
+		r.updateState(*state)
 		b.resolvers.Store(target.URL.Host, r)
 	}
 
@@ -46,7 +46,7 @@ func (b *Builder) Scheme() string {
 }
 
 func (b *Builder) UpdateStates(instances []*registry.ServiceInstance) {
-	states := make(map[string]resolver.State, len(instances))
+	states := make(map[string]*resolver.State, len(instances))
 	for _, instance := range instances {
 		ep, err := endpoint.ParseEndpoint(instance.Endpoint)
 		if err != nil {
@@ -57,7 +57,7 @@ func (b *Builder) UpdateStates(instances []*registry.ServiceInstance) {
 		if state, ok := states[instance.ID]; ok {
 			state.Addresses = append(state.Addresses, resolver.Address{Addr: ep.Address()})
 		} else {
-			states[instance.ID] = resolver.State{Addresses: []resolver.Address{{Addr: ep.Address()}}}
+			states[instance.ID] = &resolver.State{Addresses: []resolver.Address{{Addr: ep.Address()}}}
 		}
 	}
 
@@ -67,7 +67,7 @@ func (b *Builder) UpdateStates(instances []*registry.ServiceInstance) {
 
 	b.resolvers.Range(func(key, value any) bool {
 		r := value.(*Resolver)
-		r.updateState(states[r.target.URL.Host])
+		r.updateState(*states[r.target.URL.Host])
 		return true
 	})
 }
--- transport/grpc/internal/resolver/discovery/builder.go
+++ transport/grpc/internal/resolver/discovery/builder.go
@@ -13,14 +13,14 @@ const scheme = "discovery"
 
 type Builder struct {
 	rw        sync.RWMutex
-	states    map[string]resolver.State
+	states    map[string]*resolver.State
 	resolvers sync.Map
 }
 
 var _ resolver.Builder = &Builder{}
 
 func NewBuilder() *Builder {
-	return &Builder{states: make(map[string]resolver.State)}
+	return &Builder{states: make(map[string]*resolver.State)}
 }
 
 func (b *Builder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
@@ -29,7 +29,7 @@ func (b *Builder) Build(target resolver.Target, cc resolver.ClientConn, opts res
 	b.rw.RUnlock()
 
 	r := &Resolver{builder: b, target: target, cc: cc}
-	r.updateState(state)
+	r.updateState(*state)
 	b.resolvers.Store(target.URL.Host, r)
 
 	return r, nil
@@ -40,7 +40,7 @@ func (b *Builder) Scheme() string {
 }
 
 func (b *Builder) UpdateStates(instances []*registry.ServiceInstance) {
-	states := make(map[string]resolver.State, len(instances))
+	states := make(map[string]*resolver.State, len(instances))
 	for _, instance := range instances {
 		ep, err := endpoint.ParseEndpoint(instance.Endpoint)
 		if err != nil {
@@ -52,7 +52,7 @@ func (b *Builder) UpdateStates(instances []*registry.ServiceInstance) {
 			if state, ok := states[service]; ok {
 				state.Addresses = append(state.Addresses, resolver.Address{Addr: ep.Address(), ServerName: service})
 			} else {
-				states[service] = resolver.State{Addresses: []resolver.Address{{Addr: ep.Address(), ServerName: service}}}
+				states[service] = &resolver.State{Addresses: []resolver.Address{{Addr: ep.Address(), ServerName: service}}}
 			}
 		}
 	}
@@ -63,7 +63,7 @@ func (b *Builder) UpdateStates(instances []*registry.ServiceInstance) {
 
 	b.resolvers.Range(func(key, value any) bool {
 		r := value.(*Resolver)
-		r.updateState(states[r.target.URL.Host])
+		r.updateState(*states[r.target.URL.Host])
 		return true
 	})
 }
