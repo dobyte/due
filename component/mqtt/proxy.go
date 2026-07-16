5068b0992b0fae58d8de01e0a52012085700a4ed
fuxiao <yuebanfuxiao@gmail.com>
2026-07-16 11:15:41 +0800

    Optimizing mqtt

--- component/mqtt/proxy.go
+++ component/mqtt/proxy.go
@@ -25,10 +25,41 @@ func (p *Proxy) Publish(topic string, payload []byte, retain bool, qos byte) err
 }
 
 // Subscribe 订阅主题
-func (p *Proxy) Subscribe(filter string, subscriptionId int, handler SubscribeHandler) error {
+func (p *Proxy) Subscribe(filter string, subscriptionID int, handler SubscribeHandler) error {
 	if p.server.server == nil {
 		return errors.ErrServerClosed
 	} else {
-		return p.server.server.Subscribe(filter, subscriptionId, handler)
+		return p.server.server.Subscribe(filter, subscriptionID, handler)
+	}
+}
+
+// Unsubscribe 取消订阅主题
+func (p *Proxy) Unsubscribe(filter string, subscriptionID int) error {
+	if p.server.server == nil {
+		return errors.ErrServerClosed
+	} else {
+		return p.server.server.Unsubscribe(filter, subscriptionID)
+	}
+}
+
+// Client 获取客户端
+func (p *Proxy) Client(clientID string) (*mqtt.Client, bool) {
+	if p.server.server == nil {
+		return nil, false
+	} else {
+		return p.server.server.Clients.Get(clientID)
+	}
+}
+
+// Clients 获取所有客户端
+func (p *Proxy) Clients() (map[string]*mqtt.Client, error) {
+	if p.server.server == nil {
+		return nil, errors.ErrServerClosed
+	} else {
+		clients := p.server.server.Clients.GetAll()
+
+		delete(clients, mqtt.InlineClientId)
+
+		return clients, nil
 	}
 }
