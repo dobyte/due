65f58fbe85a34a3756e4380547dbbaca87207350
fuxiao <yuebanfuxiao@gmail.com>
2026-09-05 10:55:59 +0800

    Refactor kcp client test message sending

--- network/kcp/client_test.go
+++ network/kcp/client_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/dobyte/due/v2/network"
 	"github.com/dobyte/due/v2/packet"
 	"github.com/dobyte/due/v2/utils/xrand"
+	"github.com/dobyte/due/v2/utils/xtime"
 )
 
 func TestClient_Simple(t *testing.T) {
@@ -50,32 +51,36 @@ func TestClient_Simple(t *testing.T) {
 	defer ticker.Stop()
 
 	for {
-		select {
-		case <-ticker.C:
-			msg, err := packet.PackMessage(&packet.Message{
-				Seq:    1,
-				Route:  1,
-				Buffer: []byte("hello server~~"),
-			})
-			if err != nil {
-				log.Errorf("pack message failed: %v", err)
-				continue
-			}
+		t, ok := <-ticker.C
+		if !ok {
+			return
+		}
 
-			if err = conn.Push(msg); err != nil {
-				log.Errorf("push message failed: %v", err)
-				return
-			}
+		buffer := fmt.Appendf([]byte{}, "%s: hello server~~", t.Format(xtime.DateTime))
 
-			counter++
+		msg, err := packet.PackMessage(&packet.Message{
+			Seq:    1,
+			Route:  1,
+			Buffer: buffer,
+		})
+		if err != nil {
+			log.Errorf("pack message failed: %v", err)
+			continue
+		}
 
-			if counter%5 == 0 {
-				conn.Close()
-			}
+		if err = conn.Push(msg); err != nil {
+			log.Errorf("push message failed: %v", err)
+			return
+		}
 
-			if counter >= 200 {
-				return
-			}
+		counter++
+
+		if counter%5 == 0 {
+			conn.Close()
+		}
+
+		if counter >= 200 {
+			return
 		}
 	}
 }
@@ -167,29 +172,27 @@ func doPressureTest(c int, n int, size int) {
 			defer conn.Close(true)
 
 			for {
-				select {
-				case _, ok := <-chMsg:
-					if !ok {
-						return
-					}
-
-					msg, err := packet.PackMessage(&packet.Message{
-						Seq:    1,
-						Route:  1,
-						Buffer: buffer,
-					})
-					if err != nil {
-						log.Errorf("pack message failed: %v", err)
-						return
-					}
-
-					if err = conn.Push(msg); err != nil {
-						log.Errorf("push message failed: %v", err)
-						return
-					}
-
-					atomic.AddInt64(&totalSent, 1)
+				_, ok := <-chMsg
+				if !ok {
+					return
 				}
+
+				msg, err := packet.PackMessage(&packet.Message{
+					Seq:    1,
+					Route:  1,
+					Buffer: buffer,
+				})
+				if err != nil {
+					log.Errorf("pack message failed: %v", err)
+					return
+				}
+
+				if err = conn.Push(msg); err != nil {
+					log.Errorf("push message failed: %v", err)
+					return
+				}
+
+				atomic.AddInt64(&totalSent, 1)
 			}
 		}(conn)
 	}
